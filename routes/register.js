f9a2f1de2de9bebfbac74d72ba0b0070793b1838
riqui <riquipipe@hotmail.com>
2023-05-30 15:52:03 -0500

    Funciona Register a la DB backend

--- routes/register.js
+++ routes/register.js
@@ -2,37 +2,37 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-const registeredUsers = [];
 
+async function insertarRegistro(datos) {
+	const { nombre, apellido, telefono, correo, password} = datos;
+	console.log('Received data (async):', { nombre, apellido, telefono, correo, password });
+	try {
+		const existeCorreoClienteQuery = 'SELECT COUNT(*) FROM "schema"."Cliente" where "Email" = $1'
+		const { rows: correoExistente } = await pool.query(existeCorreoClienteQuery, [correo]);
+
+		if (parseInt(correoExistente[0].count) > 0){
+			console.log('El correo ya esta registrado');
+			return;
+		}
+		await pool.query('ALTER TABLE "schema"."Cliente" DISABLE TRIGGER ALL;');
+
+		const insertQuery = 'INSERT INTO "schema"."Cliente" ("Name", "Email", "Password", "surname", "Telefono") VALUES ($1, $2, $3,$4, $5)'
+		await pool.query(insertQuery, [nombre, correo, password, apellido, telefono]);
+		console.log('Registro insertado correctamente');
+
+		await pool.query('ALTER TABLE "schema"."Cliente" ENABLE TRIGGER ALL;');
+		//res.status(200).json({ response: responseValue });
+	} catch (error) {
+		console.error('Error al insertar el registro: ', error);
+	}
+}
 router.post('/', (req, res) => {
 	const { nombre, apellido, telefono, correo, password} = req.body;
-	registeredUsers.push({nombre, apellido, telefono, correo, password});
 	console.log('Received data:', { nombre, apellido, telefono, correo, password });
 
 	const responseValue = 1; // Or 0 based on your requirements
 
-	async function insertarRegistro(datos) {
-		const { nombre, apellido, telefono, correo, password} = datos;
-
-		try {
-			const existeCorreoClienteQuery = 'SELECT COUNT(*) FROM "schema"."Cliente" where "Email" = $1'
-			const { rows: correoExistente } = await pool.query(existeCorreoClienteQuery, [correo]);
-
-			if (parseInt(correoExistente[0].count) > 0){
-				console.log('El correo ya esta registrado');
-				return;
-			}
-
-			const insertQuery = 'INSERT INTO "schema"."Cliente" ("Name", "Email", "Password", "surname", "Telefono") VALUES ($1, $2, $3,$4, $5)'
-			await pool.query(insertQuery, [nombre, correo, password, apellido, telefono]);
-			console.log('Registro insertado correctamente');
-			//res.status(200).json({ response: responseValue });
-		} catch (error) {
-			console.error('Error al insertar el registro: ', error);
-		}
-	}
-
-	insertarRegistro(registeredUsers);
+	insertarRegistro({ nombre, apellido, telefono, correo, password});
 
 });
 
